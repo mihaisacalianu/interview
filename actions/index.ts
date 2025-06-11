5434b8863376b1b43cc0ee96785f3d1f98640135
mihaisacalianu <mihaisacalianu@gmail.com>
2025-06-11 11:10:02 +0100

    built basic locations card with basic design

--- actions/index.ts
+++ actions/index.ts
@@ -1,7 +1,21 @@
 "use server"
 
 export const getEpisodes = async () => {
-  const response = await fetch("https://rickandmortyapi.com/api/episode");
-  const data = await response.json();
-  return data.results;
+  try{
+    const response = await fetch("https://rickandmortyapi.com/api/episode");
+    const data = await response.json();
+    return data.results;
+  }catch(error){
+    console.log('There was an error',error);
+  }
+}
+
+export const getLocations = async () => {
+  try{
+    const response = await fetch('https://rickandmortyapi.com/api/location');
+    const locationData = await response.json();
+    return locationData.results;
+  }catch(error){
+    console.log('There was an error',error);
+  }
 }
--- app/locations/page.tsx
+++ app/locations/page.tsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React from 'react';
+import { getLocations } from '@/actions';
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
 
-const Locations = () => {
+
+type LocationType = {
+  id: number,
+  name: string,
+  type: string,
+  dimension: string,
+  residents: string[],
+  url: string,
+  created: string
+}
+
+const Locations = async () => {
+
+  const locationData:LocationType[] = await getLocations();
+
+
+
   return (
     <div>
-      <h1>Locations</h1>
+     <div className='w-60% mx-auto flex flex-col justify-center items-center gap-4'>
+      {locationData.map((location)=>{return (
+        <Card key={location.id} className='w-[40%]'>
+        <CardHeader>
+          <CardTitle>{location.name}</CardTitle>
+          <CardDescription>Type: {location.type}</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>{location.name}: {location.dimension === "unknown" ? "dimension " + location.dimension : location.dimension }</p>
+          <ul>
+            {location.residents.map((resident)=> <li key={location.id}>{resident}</li>)}
+          </ul>
+        </CardContent>
+      </Card>
+      )})
+      }
+         </div>
     </div>
   )
 }
