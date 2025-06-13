807bc07515ec52ad5b5a0d50f458245d6c707e59
mihaisacalianu <mihaisacalianu@gmail.com>
2025-06-13 15:20:18 +0100

    restyled citizens cards and adde image to the cards

--- app/(main)/locations/(no-layout)/[id]/page.tsx
+++ app/(main)/locations/(no-layout)/[id]/page.tsx
@@ -9,6 +9,7 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import Link from "next/link";
+import Image from "next/image"
 import { getIdFromUrl } from "../../../../../lib/utils";
 
 
@@ -28,21 +29,24 @@ const LocationDetailsPage = async({ params }: PageProps) => {
     <main className='w-[80%] mx-auto mt-10'>
         <Card className='flex '>
         <CardHeader className='w-full'>
-          <CardTitle>{locationData.name}</CardTitle>
-          <CardDescription className='mb-6'>
+          <CardTitle className='text-4xl text-[#2C764C] text-center mb-3'>{locationData.name}</CardTitle>
+          <CardDescription className='text-2xl text-center'>
             <h2>Type: {locationData.type}</h2>
           </CardDescription>
         </CardHeader>
         <CardContent className='w-full mb-4'>
           <h3><strong className='text-xl my-6'>Description:</strong></h3>
           <p>Planet Dimensions: {locationData.dimension}</p>
-          <h3>Planet Residents:</h3>
+          <h3 className='text-2xl my-6 text-[#2C764C] font-bold'>Planet Citizens:</h3>
           {filteredResidents.map((resident)=>(
-              <Card className='grid grid-cols-2 mb-10 p-6 text-center w-[40%] mx-auto' key={resident.id} >
-                <CardTitle>
-                  {resident.name}
-                </CardTitle>
-                <Link href={`/characters/${resident.id}`} className='hover:bg-blue-500 text-center text-white bg-blue-400 w-[60%] mx-auto p-2 rounded-md'>View Details</Link>
+              <Card className='grid grid-cols-2 mb-6 p-4 text-center w-[35%] mx-auto' key={resident.id} >
+                <Image src={resident.image} alt="image of a planet resident" width={100} height={50} className='rounded-md'/>
+                <div aria-label="resident details container" className='flex flex-col gap-3'>
+                  <CardTitle className='text-2xl text-[#2C764C] font-bold'>
+                    {resident.name}
+                  </CardTitle>
+                  <Link href={`/characters/${resident.id}`} className='hover:bg-[#DD3A0A] text-center text-white bg-[#EB862D] w-[60%] mx-auto p-2 rounded-md'>View Details</Link>
+                </div>
               </Card>
           ))}
         </CardContent>
