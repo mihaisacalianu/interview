3302185b1bc5e71d590738357f9b889c7dee2fae
mihaisacalianu <mihaisacalianu@gmail.com>
2025-06-13 14:55:23 +0100

    finished design and look of character show page

--- app/(main)/characters/(no-layout)/[id]/page.tsx
+++ app/(main)/characters/(no-layout)/[id]/page.tsx
@@ -26,20 +26,22 @@ const CharacterPage = async({ params }: PageProps) => {
 
   return (
     <section className='w-[80%] mx-auto mt-10'>
-        <Card className='flex flex-col bg-[#EFEFEF]'>
-        <CardHeader className='w-[90%] mx-auto flex justify-center items-center'>
-          <Image src={caracterData.image} alt="main card image" width={450} height={150} className='w-[20%]'/>
-          <CardDescription className='flex'>
-          <CardTitle>{caracterData.name}</CardTitle>
+        <Card className='flex flex-col bg-[#EFEFEF] p-6'>
+        <CardHeader className='w-[100%] mx-auto flex justify-between items-center mb-10 gap-26'>
+          <Image src={caracterData.image} alt="main card image" width={550} height={250} className='w-[40%] rounded-md shadow-md'/>
+          <CardDescription className='flex flex-col w-[60%] text-xl gap-3'>
+          <CardTitle className='text-4xl'>{caracterData.name}</CardTitle>
             <h2>Species: {caracterData.species}</h2>
-            <h3 className='text-2xl text-red-600'><strong>Description:</strong></h3>
+            <h3 className='text-3xl text-[#2C764C]'><strong>Description:</strong></h3>
           <p>Gender: {caracterData.gender}</p>
           <p>Status: {caracterData.status}</p>
-          <h3>Location:</h3>
-          <Link href={caracterData.location.url}>{caracterData.location.name}</Link>
+          <div aria-label='location container' className='flex justify-between w-[50%]'>
+            <h3>Location:</h3>
+            <Link href={`/locations`} className='hover:text-[#2C764C]'>{caracterData.location.name}</Link>
+          </div>
           </CardDescription>
         </CardHeader>
-          <h2>Episodes: </h2>
+          <h2 className='text-3xl font-bold tracking-wider text-center text-[#2C764C]'>Episodes: </h2>
         <CardContent className='grid grid-cols-3 gap-3 '>
           {filteredEpisodes.map((episode)=>(
               <Card className='flex my-5 w-[90%] mx-auto p-6' key={episode.id} >
