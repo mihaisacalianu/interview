d6777e754fd825134314183f0551752cd17993dd
mihaisacalianu <mihaisacalianu@gmail.com>
2025-06-13 12:15:13 +0100

    made hero image to use calc formula 100dvh - 88px width of navBar

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
 import Image from "next/image"
 export default function Home() {
   return (
-    <div >
-      <main className="w-[100dvw] h-[100dvh] mx-auto flex flex-col">
-        <Image src="/mainBg.png" alt="hero image with ricky and morty" width={1000} height={1000} className="w-[100dvw] h-[100dvh]" priority/>
-      </main>
-    </div>
+    <main className="w-dvw h-dvh mx-auto flex flex-col">
+      <Image src="/mainBg.png" alt="hero image with ricky and morty" width={1000} height={1000} className="w-dvw h-[calc(100dvh-88px)]" priority/>
+    </main>
   );
 }
